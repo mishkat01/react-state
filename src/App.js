23625e8d2e04031fcfae7f4abdedbc1431f34968
mishkat01 <mishkatislam007@gmail.com>
2025-08-07 22:54:01 +0600

    state function called

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import BioData from './components/BioData';
 
+import { useState } from 'react';
+
 function App() {
-  let counter =0;
+
   
+  const [dynamicValue, setDynamicvalue]= useState(0);
   const decrease= ()=>{
-  
+             setDynamicvalue(dynamicValue - 1);
   }
   
   const increase= ()=>{
-    
+
+    setDynamicvalue(dynamicValue +1);
+
   }
   return (
     <div className="App">
           <p1>
-            in crease counter by {counter}
+            in crease counter by {dynamicValue}
           </p1>
           <button onClick={increase}>
             click to increase +1
